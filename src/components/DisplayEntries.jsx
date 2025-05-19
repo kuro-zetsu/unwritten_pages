4d4a682aaba49ad2d526c673a042658b5f51e40c
Reagan Nyauma <jreagan2525@gmail.com>
2025-05-19 06:43:16 +0300

    added a message for when the user tries to view important entries without marking any as important first

--- src/components/DisplayEntries.jsx
+++ src/components/DisplayEntries.jsx
@@ -1,5 +1,6 @@
 import Entry from "./Entry";
 import styles from "../assets/css/DisplayEntries.module.css";
+import { Star } from "lucide-react";
 
 function DisplayEntries({
   posts,
@@ -14,8 +15,9 @@ function DisplayEntries({
       <h1>{showImportant ? "pinned thoughts" : "my journal"}</h1>
       <hr />
       <div className={styles.entries}>
-        {showImportant
-          ? // If important is true, display only important entries
+        {showImportant ? (
+          // If important is true, display only important entries
+          importantEntries.length ? (// If there are important entries to display, display them
             importantEntries
               .slice()
               .reverse()
@@ -32,23 +34,37 @@ function DisplayEntries({
                   />
                 </div>
               ))
-          : // Otherwise, display all journal entries
-            posts
-              .slice()
-              .reverse()
-              .map((post) => (
-                <div key={post.id} className={styles.entry}>
-                  <Entry
-                    id={post.id}
-                    title={post.title}
-                    body={post.body}
-                    important={post.important}
-                    changeImportant={changeImportant}
-                    deleteEntry={deleteEntry}
-                    edit={editing}
-                  />
-                </div>
-              ))}
+          ) : (// If there are no important entries to display, let the user know how to mark an entry as important
+            <>
+              <div></div>
+              <div className={styles["no-entries"]}>
+                <p>
+                  Not a single thought has been pinned to the wall. Click the{" "}
+                  <Star size={18} color='rgb(11, 255, 255)' /> on any entry to
+                  spotlight your favourites.
+                </p>
+              </div>
+            </>
+          )
+        ) : (
+          // Otherwise, display all journal entries
+          posts
+            .slice()
+            .reverse()
+            .map((post) => (
+              <div key={post.id} className={styles.entry}>
+                <Entry
+                  id={post.id}
+                  title={post.title}
+                  body={post.body}
+                  important={post.important}
+                  changeImportant={changeImportant}
+                  deleteEntry={deleteEntry}
+                  edit={editing}
+                />
+              </div>
+            ))
+        )}
       </div>
     </div>
   );
